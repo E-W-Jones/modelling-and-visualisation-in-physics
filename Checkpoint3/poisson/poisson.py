97345324714ac03295709050fd06592550c9c811
Evan Jones <s1857441@ed.ac.uk>
2023-03-31 15:57:34 +0100

    workshop

--- Checkpoint3/poisson/poisson.py
+++ Checkpoint3/poisson/poisson.py
@@ -4,75 +4,80 @@ rng = np.random.default_rng()
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1.axes_grid import ImageGrid
 
 from tqdm import tqdm
 
 from itertools import product
 
-class PoissonSolver:
-    def __init__(self, N, rho=None):
-        self.N = N
-        if rho is None:
-            self.rho = np.zeros([N, N, N])
-            self.rho[N//2, N//2, N//2] = 1
-        else:
-            self.rho = rho
 
-        self.phi = self.rho / 6
-
-        self.edge = np.ones([N, N, N], dtype=bool)
-        self.edge[1:-1, 1:-1, 1:-1] = False
-
-    def run(self, tolerance):
-        difference = np.ones([self.N-2, self.N-2, self.N-2])
-        condition = np.any(np.abs(difference) > tolerance)
-        for i in tqdm(range(2000000)):
-            phi_new    = ( self.phi[ :-2, 1:-1, 1:-1]
-                         + self.phi[2:  , 1:-1, 1:-1]
-                         + self.phi[1:-1,  :-2, 1:-1]
-                         + self.phi[1:-1, 2:  , 1:-1]
-                         + self.phi[1:-1, 1:-1,  :-2]
-                         + self.phi[1:-1, 1:-1, 2:  ]
-                         + self.rho[1:-1, 1:-1, 1:-1]) / 6
-        #     # for i, j, k in product(range(1, self.N-1), repeat=3):
-        #     #     difference[i-1, j-1, k-1] = ( self.phi[i-1, j, k]
-        #     #                  + self.phi[i+1, j, k]
-        #     #                  + self.phi[i, j-1, k]
-        #     #                  + self.phi[i, j+1, k]
-        #     #                  + self.phi[i, j, k-1]
-        #     #                  + self.phi[i, j, k+1]
-        #     #                  ) / 6
-            difference = phi_new - self.phi[1:-1, 1:-1, 1:-1]
-            condition = np.any(np.abs(difference) > tolerance)
-            self.phi = np.pad(phi_new, 1)
-        #print(np.min(difference), np.mean(difference), np.max(difference))
-        #print(self.phi)
-        np.savetxt(f'poisson_output_N{self.N}.txt', self.phi.flatten())
-        self.phi = np.loadtxt(f'poisson_output_N{self.N}.txt').reshape(self.N, self.N, self.N)
-        Ex, Ey, Ez = np.gradient(-self.phi)
-        plt.imshow(np.log(self.phi[:, :, self.N//2]))
-        norm = np.hypot(Ex, Ey, Ez)
-        Ex /= norm
-        Ey /= norm
-        plt.quiver(Ex[:, :, self.N//2], Ey[:, :, self.N//2], angles='xy')
+class VisualisePotential:
+    def __init__(self, phi, Ex, Ey, Ez):
+        self.N = phi.shape[0]
+        self.x, self.y, self.z = [x-self.N/2 for x in np.mgrid[0:self.N, 0:self.N, 0:self.N]]
+        self.phi = phi
+        self.Ex, self.Ey, self.Ez = Ex, Ey, Ez
+        
+        self.fig = plt.figure()
+        grid = ImageGrid(self.fig, 111, (1, 3), cbar_mode="single", axes_pad=0.7, label_mode='all')
+        self.axes = grid.axes_all
+        self.cbar_ax = grid.cbar_axes[0]
+        for ax, (x, y) in zip(self.axes, ['yz', 'xz', 'xy']):
+            ax.set(xlabel=x, ylabel=y)
+        
+        self.plot_phi()
+        #self.plot_E()
         plt.show()
-
-        N = self.N
-        x, y, z = [x.flatten() for x in np.mgrid[-N//2:N//2, -N//2:N//2, -N//2:N//2]]
-        r = np.sqrt(x**2 + y**2 + z**2)
-        plt.plot(r, norm.flatten(), 'o')
-        plt.show()
-        plt.loglog(r, norm.flatten(), 'o')
-        plt.show()
-        # plt.plot(norm[N//2:, 0, 0])
-        # plt.show()
-        # plt.loglog(norm[N//2:, 0, 0])
-        # plt.show()
-
-
+    
+    def plot_phi(self):
+        vmin, vmax = np.nanmin(self.phi), np.nanmax(self.phi)
+        kwargs = {"extent": (-self.N/2, self.N/2, -self.N/2, self.N/2),
+                  "norm": LogNorm(vmin=vmin, vmax=vmax)
+                  }
+        im = self.axes[0].imshow(self.phi[self.N//2, :, :], **kwargs)
+        self.axes[1].imshow(self.phi[:, self.N//2, :], **kwargs)
+        self.axes[2].imshow(self.phi[:, :, self.N//2], **kwargs)
+        #im = self.axes[0].pcolormesh(self.y[self.N//2, :, :],
+        #                             self.z[self.N//2, :, :],
+        #                             self.phi[self.N//2, :, :],
+        #                             **kwargs)
+        #self.axes[1].pcolormesh(self.x[:, self.N//2, :],
+        #                        self.z[:, self.N//2, :],
+        #                        self.phi[:, self.N//2, :],
+        #                        **kwargs)
+        #self.axes[2].pcolormesh(self.x[:, :, self.N//2],
+        #                        self.y[:, :, self.N//2],
+        #                        self.phi[:, :, self.N//2],
+        #                        **kwargs)
+                                
+        plt.colorbar(im, cax=self.cbar_ax)
+    
+    def plot_E(self):
+        kwargs = {"angles": "xy"}
+        self.axes[0].quiver(self.y[self.N//2, :, :], self.z[self.N//2, :, :],
+                            self.Ey[self.N//2, :, :], self.Ez[self.N//2, :, :],
+                            **kwargs)
+        self.axes[1].quiver(self.x[:, self.N//2, :], self.z[:, self.N//2, :],
+                            self.Ex[:, self.N//2, :], self.Ez[:, self.N//2, :],
+                            **kwargs)
+        self.axes[2].quiver(self.x[:, :, self.N//2], self.y[:, :, self.N//2],
+                            self.Ex[:, :, self.N//2], self.Ey[:, :, self.N//2],
+                            **kwargs)
+    
+    @staticmethod
+    def from_point_charge(N):
+        x, y, z = [x-N/2 for x in np.mgrid[:N, :N, :N]]
+        r = np.sqrt(x*x + y*y + z*z)
+        phi = 1 / (4 * np.pi * r)
+        phi[np.isinf(phi)] = np.nan
+        E = 1 / (4 * np.pi * r**3)
+        phi[np.isinf(E)] = np.nan
+        Ex, Ey, Ez = E*x, E*y, E*z
+        return VisualisePotential(phi, Ex, Ey, Ez)
+        
 def main():
-    solver = PoissonSolver(25)
-    solver.run(1e-9)
+    VisualisePotential.from_point_charge(4).plot_phi()
 
 if __name__ == "__main__":
     main()
