5557440cde9fe8e79848604d90cdbf0d9aa2ba48
E-W-Jones <evan_w_jones@hotmail.com>
2023-04-04 20:01:12 +0100

    uhhhhh

--- Checkpoint3/poisson/poisson.py
+++ Checkpoint3/poisson/poisson.py
@@ -1,41 +1,34 @@
 import argparse
 import numpy as np
-rng = np.random.default_rng()
-
+from itertools import product
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from matplotlib.colors import LogNorm
-from mpl_toolkits.axes_grid1.axes_grid import ImageGrid
-
 from tqdm import tqdm
 
-from itertools import product
+from visualisation import VisualisePotential
 
-from scipy.optimize import curve_fit
+class PoissonSolver:
+    def __init__(self, N=51, tolerance=1e-6, rho=None, verbose=False):
+        if rho is not None:
+            self.rho = rho
+            self.N = rho.shape[0]
+        else:
+            self.N = N
+            self.rho = np.zeros([self.N, self.N, self.N])
+            self.rho[N//2, N//2, N//2] = 1
 
-class PoissonSolverJacobi:
-    def __init__(self, N, tolerance):
-        self.N = N
         self.tolerance = tolerance
         
         self.phi_new = np.zeros([self.N, self.N, self.N])
         self.phi_old = np.zeros([self.N, self.N, self.N])
 
-        self.rho = np.zeros([self.N, self.N, self.N])
-        self.rho[N//2, N//2, N//2] = 1
+        self.verbose = verbose
 
     def iterate(self):
-        for i, j, k in product(range(1, self.N-1), repeat=3):
-            self.phi_new[i, j, k] = ( self.phi_old[i-1, j, k]
-                                    + self.phi_old[i+1, j, k]
-                                    + self.phi_old[i, j-1, k]
-                                    + self.phi_old[i, j+1, k]
-                                    + self.phi_old[i, j, k-1]
-                                    + self.phi_old[i, j, k+1]
-                                    + self.rho[i, j, k] 
-                                    ) / 6
+        raise NotImplementedError
+
     def check_converged(self):
-        print("max error:", np.max(np.abs(self.phi_new - self.phi_old)))
+        if self.verbose:
+            print("max error:", np.max(np.abs(self.phi_new - self.phi_old)))
         return np.all(np.abs(self.phi_new - self.phi_old) < self.tolerance)
 
     def save_output(self, filename=None):
@@ -57,7 +50,20 @@ class PoissonSolverJacobi:
         # E = -grad phi, take grad(-phi) for ease
         self.Ex, self.Ey, self.Ez = np.gradient(-self.phi)
 
-class PoissonSolverJacobiNumpy(PoissonSolverJacobi):
+
+class PoissonSolverJacobi(PoissonSolver):
+    def iterate(self):
+        for i, j, k in product(range(1, self.N-1), repeat=3):
+            self.phi_new[i, j, k] = ( self.phi_old[i-1, j, k]
+                                    + self.phi_old[i+1, j, k]
+                                    + self.phi_old[i, j-1, k]
+                                    + self.phi_old[i, j+1, k]
+                                    + self.phi_old[i, j, k-1]
+                                    + self.phi_old[i, j, k+1]
+                                    + self.rho[i, j, k] 
+                                    ) / 6
+
+class PoissonSolverJacobiNumpy(PoissonSolver):
     def iterate(self):
         self.phi_new[1:-1, 1:-1, 1:-1] = ( self.phi_old[ :-2, 1:-1, 1:-1]
                                          + self.phi_old[2:  , 1:-1, 1:-1]
@@ -69,173 +75,17 @@ class PoissonSolverJacobiNumpy(PoissonSolverJacobi):
                                          ) / 6
 
 class PoissonSolverGaussSteidel:
-    def __init__(self, N, tolerance):
-        self.N = N
-        self.tolerance = tolerance
-        
-        self.phi = np.zeros([self.N, self.N, self.N])
-        self.phi_old = np.zeros([self.N, self.N, self.N])
-
-        self.rho = np.zeros([self.N, self.N, self.N])
-        self.rho[N//2, N//2, N//2] = 1
-
     def iterate(self):
         for i, j, k in product(range(1, self.N-1), repeat=3):
-            self.phi[i, j, k] = ( self.phi[i-1, j, k]
-                                + self.phi[i+1, j, k]
-                                + self.phi[i, j-1, k]
-                                + self.phi[i, j+1, k]
-                                + self.phi[i, j, k-1]
-                                + self.phi[i, j, k+1]
-                                + self.rho[i, j, k] 
-                                ) / 6
+            self.phi_new[i, j, k] = ( self.phi_new[i-1, j, k]
+                                    + self.phi_new[i+1, j, k]
+                                    + self.phi_new[i, j-1, k]
+                                    + self.phi_new[i, j+1, k]
+                                    + self.phi_new[i, j, k-1]
+                                    + self.phi_new[i, j, k+1]
+                                    + self.rho[i, j, k] 
+                                    ) / 6
 
-    def check_converged(self):
-        print("max error:", np.max(np.abs(self.phi - self.phi_old)))
-        return np.all(np.abs(self.phi - self.phi_old) < self.tolerance)
-
-    def save_output(self, filename=None):
-        if filename is None:
-            filename = f"poisson_output_N{self.N}_tol{self.tolerance:.1e}.txt"
-        header = "phi | Ex | Ey | Ez"
-        save_arrays = [self.phi, self.Ex, self.Ey, self.Ez]
-        save_array = np.stack([arr.flatten() for arr in save_arrays]).T
-        np.savetxt(filename, save_array, header=header)
-        return filename
-
-    def solve(self):
-        self.iterate()
-        while not self.check_converged():
-            self.phi_old[...] = self.phi[...]
-            self.iterate()
-
-        # E = -grad phi, take grad(-phi) for ease
-        self.Ex, self.Ey, self.Ez = np.gradient(-self.phi)
-
-class VisualisePotential:
-    def __init__(self, phi, Ex, Ey, Ez):
-        self.N = phi.shape[0]
-        # Neat way to get the integer grid spacing, origin at N//2
-        self.x, self.y, self.z = np.mgrid[-self.N//2:self.N//2,
-                                          -self.N//2:self.N//2,
-                                          -self.N//2:self.N//2] + self.N % 2
-
-        self.phi = np.copy(phi)
-        self.Ex, self.Ey, self.Ez = np.copy(Ex), np.copy(Ey), np.copy(Ez)
-        # check if its 2d or 3d, if 2d extrude it into 3d
-        self.check_3d()
-        self.phi[self.phi == 0] = np.nan
-        
-        self.plot_fields()
-        # self.plot_analysis()
-
-    def extrude(self, arr):
-        return np.tile(arr[:, :, None], (1, 1, self.N))
-
-    def check_3d(self):
-        # check if its 2d or 3d, if 2d extrude it into 3d
-        if len(self.phi.shape) == 2:
-            self.phi = self.extrude(self.phi)
-        if len(self.Ex.shape) == 2:
-            self.Ex = self.extrude(self.Ex)
-        if len(self.Ey.shape) == 2:
-            self.Ey = self.extrude(self.Ey)
-        if len(self.Ez.shape) == 2:
-            self.Ez = self.extrude(self.Ez)
-
-    def plot_fields(self):
-        self.fig = plt.figure()
-        grid = ImageGrid(self.fig, 111, (1, 3), cbar_mode="single", axes_pad=0.7, label_mode='all')
-        self.axes = grid.axes_all
-        self.cbar_ax = grid.cbar_axes[0]
-        for ax, (x, y) in zip(self.axes, ['yz', 'xz', 'xy']):
-            ax.set(xlabel=x, ylabel=y)
-        
-        self.plot_phi()
-        self.plot_E()
-        plt.show()
-
-    def plot_analysis(self):
-        x = self.x[self.N//2:, self.N//2, self.N//2]
-        y = self.y[self.N//2:, self.N//2, self.N//2]
-        z = self.z[self.N//2:, self.N//2, self.N//2]
-        r = np.sqrt(x*x + y*y + z*z)
-        logr = np.log(r[1:])  # skip the first one as log0 = -inf
-
-        phi = self.phi[self.N//2:, self.N//2, self.N//2]
-        logphi = np.log(phi[1:])
-        # take up to logr = 2 - this can and probaby should be tweaked
-        n = np.count_nonzero(logr <= 2)
-        mphi, cphi = self.fit_line(logr[:n], logphi[:n])
-
-        Ex = self.Ex[self.N//2:, self.N//2, self.N//2]
-        Ey = self.Ey[self.N//2:, self.N//2, self.N//2]
-        Ez = self.Ez[self.N//2:, self.N//2, self.N//2]
-        E = np.sqrt(Ex*Ex + Ey*Ey + Ez*Ez)
-        logE = np.log(E[1:])
-        mE, cE = self.fit_line(logr[:n], logE[:n])
-
-        plt.plot(logr, logphi, label=r"potential, $\phi$", c="C0")
-        plt.plot(logr, mphi*logr + cphi, label=f"Line with $m={mphi:.2f}$, $c={cphi:.2f}$", c="C0", ls="--")
-        plt.plot(logr, logE, label=r"E-field, $|\mathbf{E}|$", c="C1")
-        plt.plot(logr, mE*logr + cE, label=f"Line with $m={mE:.2f}$, $c={cE:.2f}$", c="C1", ls="--")
-        plt.xlabel(r"$\log(r)$")
-        plt.ylabel(r"$\log(\phi)$ or $\log(|\mathbf{E}|)$")
-        plt.legend()
-        plt.show()
-
-    def fit_line(self, x, y):
-        (m, c), _ = curve_fit(lambda x, m, c: m*x+c, x, y)
-        return m, c
-
-
-    def plot_phi(self):
-        vmin, vmax = np.nanmin(self.phi), np.nanmax(self.phi)
-        kwargs = {"extent": (-self.N/2, self.N/2, -self.N/2, self.N/2),
-                  "norm": LogNorm(vmin=vmin, vmax=vmax)
-                  }
-        im = self.axes[0].imshow(self.phi[self.N//2, :, :], **kwargs)
-        self.axes[1].imshow(self.phi[:, self.N//2, :], **kwargs)
-        self.axes[2].imshow(self.phi[:, :, self.N//2], **kwargs)
-
-        plt.colorbar(im, cax=self.cbar_ax)
-    
-    def plot_E(self):
-        kwargs = {"angles": "xy"}
-
-        print(self.Ey)
-        # print("looking at the x slice of By, Bz")
-        # print(np.count_nonzero(self.Ey[self.N//2, :, :]))
-        # print(np.count_nonzero(self.Ez[self.N//2, :, :]))
-        self.axes[0].quiver(self.y[self.N//2, :, :], self.z[self.N//2, :, :],
-                            self.Ey[self.N//2, :, :], self.Ez[self.N//2, :, :],
-                            **kwargs)
-        # self.axes[1].quiver(self.x[:, self.N//2, :], self.z[:, self.N//2, :],
-        #                     self.Ex[:, self.N//2, :], self.Ez[:, self.N//2, :],
-        #                     **kwargs)
-        # self.axes[2].quiver(self.x[:, :, self.N//2], self.y[:, :, self.N//2],
-        #                     self.Ex[:, :, self.N//2], self.Ey[:, :, self.N//2],
-        #                     **kwargs)
-    
-    @staticmethod
-    def from_point_charge(N):
-        # Want integer spacing, w/ 0 at N//2, N//2, N//2
-        x, y, z = np.mgrid[-N//2:N//2, -N//2:N//2, -N//2:N//2] + N % 2
-        r = np.sqrt(x*x + y*y + z*z)
-        phi = 1 / (4 * np.pi * r)
-        phi[np.isinf(phi)] = np.nan
-        E = 1 / (4 * np.pi * r**3)
-        phi[np.isinf(E)] = np.nan
-        Ex, Ey, Ez = E*x, E*y, E*z
-        return VisualisePotential(phi, Ex, Ey, Ez)
-
-    @staticmethod
-    def from_file(filename):
-        phi_flat, Ex_flat, Ey_flat, Ez_flat = np.loadtxt(filename, unpack=True)
-        N = round(phi_flat.size ** (1/3))
-        print("update from_file to deal w/ 2d AND 3d please x")
-        flat_arrs = phi_flat, Ex_flat, Ey_flat, Ez_flat
-        return VisualisePotential(*[arr.reshape(N, N, N) for arr in flat_arrs])
 
 class PoissonSolverJacobiBField:
     def __init__(self, N, tolerance):
@@ -291,18 +141,10 @@ class PoissonSolverJacobiBField:
         self.Bz =  self.Bx * 0  # 0s in the right shape
         
 def main():
-    # JacobiNumpy is fastest
-    #filename = "output_101_-9.txt"
-    # point_charge = PoissonSolverJacobiNumpy(101, 1e-9)
-    # point_charge.solve()
-    # point_charge.save_output(filename)
-    #VisualisePotential.from_file(filename)
-    #VisualisePotential.from_file("gauss_steidel_51_-6.txt")
-    #VisualisePotential.from_file("jacobi_numpy_51_-6.txt")
-    
-    wire = PoissonSolverJacobiBField(51, 1e-9)
-    wire.solve()
-    VisualisePotential(wire.A, wire.Bx, wire.By, wire.Bz)
+    point_charge = PoissonSolverJacobiNumpy()
+    point_charge.solve()
+    VisualisePotential.from_solver(point_charge)
+
 
 if __name__ == "__main__":
     main()
---
+++ Checkpoint3/poisson/visualisation.py
@@ -0,0 +1,210 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1.axes_grid import ImageGrid
+
+from scipy.optimize import curve_fit
+
+
+class VisualisePotential:
+    def __init__(self, phi, Ex, Ey, Ez, magnetic=False):
+        self.N = phi.shape[0]
+        # Neat way to get the integer grid spacing, origin at N//2
+        self.x, self.y, self.z = np.mgrid[-self.N//2:self.N//2,
+                                          -self.N//2:self.N//2,
+                                          -self.N//2:self.N//2] + self.N % 2
+
+        self.phi = np.copy(phi)
+        self.Ex, self.Ey, self.Ez = np.copy(Ex), np.copy(Ey), np.copy(Ez)
+        # check if its 2d or 3d, if 2d extrude it into 3d
+        self.check_3d()
+        self.phi[self.phi == 0] = np.nan
+
+        self.magnetic = magnetic        
+        
+        self.plot_fields()
+
+        if self.magnetic is False:
+            self.plot_electric_analysis()
+        elif self.magnetic is True:
+            self.plot_magnetic_analysis()
+        else:
+            raise ValueError("Invalid value passed to magnetic kwarg:"
+                            f"{magnetic}. Valid options are True or False.")
+
+    def extrude(self, arr):
+        return np.tile(arr[:, :, None], (1, 1, self.N))
+
+    def check_3d(self):
+        # check if its 2d or 3d, if 2d extrude it into 3d
+        if len(self.phi.shape) == 2:
+            self.phi = self.extrude(self.phi)
+        if len(self.Ex.shape) == 2:
+            self.Ex = self.extrude(self.Ex)
+        if len(self.Ey.shape) == 2:
+            self.Ey = self.extrude(self.Ey)
+        if len(self.Ez.shape) == 2:
+            self.Ez = self.extrude(self.Ez)
+
+    def plot_fields(self):
+        self.fig = plt.figure()
+        grid = ImageGrid(self.fig, 111, (1, 3), cbar_mode="single", axes_pad=0.7, label_mode='all')
+        self.axes = grid.axes_all
+        self.cbar_ax = grid.cbar_axes[0]
+        for ax, (x, y) in zip(self.axes, ['yz', 'xz', 'xy']):
+            ax.set(xlabel=x, ylabel=y)
+        
+        self.plot_phi()
+        if self.magnetic:
+            self.plot_B()
+        else:
+            self.plot_E()
+
+        plt.show()
+
+    def plot_electric_analysis(self):
+        x = self.x[self.N//2:, self.N//2, self.N//2]
+        y = self.y[self.N//2:, self.N//2, self.N//2]
+        z = self.z[self.N//2:, self.N//2, self.N//2]
+        r = np.sqrt(x*x + y*y + z*z)
+        logr = np.log(r[1:])  # skip the first one as log0 = -inf
+
+        phi = self.phi[self.N//2:, self.N//2, self.N//2]
+        logphi = np.log(phi[1:])
+        # take up to logr = 2 - this can and probaby should be tweaked
+        n = np.count_nonzero(logr <= 2)
+        mphi, cphi = self.fit_line(logr[:n], logphi[:n])
+
+        Ex = self.Ex[self.N//2:, self.N//2, self.N//2]
+        Ey = self.Ey[self.N//2:, self.N//2, self.N//2]
+        Ez = self.Ez[self.N//2:, self.N//2, self.N//2]
+        E = np.sqrt(Ex*Ex + Ey*Ey + Ez*Ez)
+        logE = np.log(E[1:])
+        mE, cE = self.fit_line(logr[:n], logE[:n])
+
+        plt.plot(logr, logphi, label=r"potential, $\phi$", c="C0")
+        plt.plot(logr, mphi*logr + cphi, label=f"Line with $m={mphi:.2f}$, $c={cphi:.2f}$ (expect $m=-1$)", c="C0", ls="--")
+        plt.plot(logr, logE, label=r"E-field, $|\mathbf{E}|$", c="C1")
+        plt.plot(logr, mE*logr + cE, label=f"Line with $m={mE:.2f}$, $c={cE:.2f}$ (expect $m=-2$)", c="C1", ls="--")
+        plt.xlabel(r"$\log(r)$")
+        plt.ylabel(r"$\log(\phi)$ or $\log(|\mathbf{E}|)$")
+        plt.legend()
+        plt.show()
+
+    def plot_magnetic_analysis(self):
+        x = self.x[self.N//2:, self.N//2, self.N//2]
+        y = self.y[self.N//2:, self.N//2, self.N//2]
+        z = self.z[self.N//2:, self.N//2, self.N//2]
+        r = np.sqrt(x*x + y*y)
+        logr = np.log(r[1:])  # skip the first one as log0 = -inf
+
+        # Az doesnt have nice power law to expose
+        #Az = self.phi[self.N//2:, self.N//2, self.N//2][1:]
+        
+        # take up to logr = 2 - this can and probaby should be tweaked
+        n = np.count_nonzero(logr <= 2)
+        #mAz, cAz = self.fit_line(logr[:n], Az[:n])
+
+        Bx = self.Ex[self.N//2:, self.N//2, self.N//2]
+        By = self.Ey[self.N//2:, self.N//2, self.N//2]
+        Bz = self.Ez[self.N//2:, self.N//2, self.N//2]
+        B = np.sqrt(Bx*Bx + By*By + Bz*Bz)
+        logB = np.log(B[1:])
+        mB, cB = self.fit_line(logr[:n], logB[:n])
+
+        #plt.plot(logr, Az, label=r"potential, $A_\mathrm{z}$", c="C0")
+        #plt.plot(logr, mphi*logr + cphi, label=f"Line with $m={mphi:.2f}$, $c={cphi:.2f}$", c="C0", ls="--")
+        plt.plot(logr, logB, label=r"B-field, $|\mathbf{B}|$", c="C1")
+        plt.plot(logr, mB*logr + cB, label=f"Line with $m={mB:.2f}$, $c={cB:.2f}$ (expect $m=-1$)", c="C1", ls="--")
+        plt.xlabel(r"$\log(r)$")
+        plt.ylabel(r"$\log(A_\mathrm{z})$ or $\log(|\mathbf{B}|)$")
+        plt.ylabel(r"$\log(|\mathbf{B}|)$")
+        plt.legend()
+        plt.show()
+
+
+    def fit_line(self, x, y):
+        (m, c), _ = curve_fit(lambda x, m, c: m*x+c, x, y)
+        return m, c
+
+
+    def plot_phi(self):
+        vmin, vmax = np.nanmin(self.phi), np.nanmax(self.phi)
+        kwargs = {"extent": (-self.N/2, self.N/2, -self.N/2, self.N/2),
+                  "norm": LogNorm(vmin=vmin, vmax=vmax)
+                  }
+        im = self.axes[0].imshow(self.phi[self.N//2, :, :], **kwargs)
+        self.axes[1].imshow(self.phi[:, self.N//2, :], **kwargs)
+        self.axes[2].imshow(self.phi[:, :, self.N//2], **kwargs)
+
+        plt.colorbar(im, cax=self.cbar_ax)
+    
+    def plot_E(self):
+        kwargs = {"angles": "xy"}
+
+        self.axes[0].quiver(self.y[self.N//2, :, :], self.z[self.N//2, :, :],
+                            self.Ey[self.N//2, :, :], self.Ez[self.N//2, :, :],
+                            **kwargs)
+        self.axes[1].quiver(self.x[:, self.N//2, :], self.z[:, self.N//2, :],
+                            self.Ex[:, self.N//2, :], self.Ez[:, self.N//2, :],
+                            **kwargs)
+        self.axes[2].quiver(self.x[:, :, self.N//2], self.y[:, :, self.N//2],
+                            self.Ex[:, :, self.N//2], self.Ey[:, :, self.N//2],
+                            **kwargs)
+
+    def plot_B(self):
+        kwargs = {"angles": "xy"}
+
+        self.axes[2].quiver(self.x[:, :, self.N//2], self.y[:, :, self.N//2],
+                            self.Ex[:, :, self.N//2], self.Ey[:, :, self.N//2],
+                            **kwargs)        
+
+    @staticmethod
+    def from_point_charge(N):
+        # Want integer spacing, w/ 0 at N//2, N//2, N//2
+        x, y, z = np.mgrid[-N//2:N//2, -N//2:N//2, -N//2:N//2] + N % 2
+        r = np.sqrt(x*x + y*y + z*z)
+        phi = 1 / (4 * np.pi * r)
+        phi[np.isinf(phi)] = np.nan
+        E = 1 / (4 * np.pi * r**3)
+        E[np.isinf(E)] = np.nan
+        Ex, Ey, Ez = E*x, E*y, E*z
+        return VisualisePotential(phi, Ex, Ey, Ez)
+
+    @staticmethod
+    def from_thin_wire(N):
+        # Want integer spacing, w/ 0 at N//2, N//2, N//2
+        x, y, z = np.mgrid[-N//2:N//2, -N//2:N//2, -N//2:N//2] + N % 2
+        r = np.sqrt(x*x + y*y)
+        Az = np.log((np.sqrt(N*N + r*r) + N) / r) / (2 * np.pi)
+        Az[np.isinf(Az)] = np.nan
+        B = 1 / (2 * np.pi * r)
+        B[np.isinf(B)] = np.nan
+        Bx, By, Bz = -B*y/r, B*x/r, 0*z
+        # Something thats confusing me is I was expecting to see Bx, By in the
+        # vector plots, however at these they are going directly into/out of the plane
+        # so shouldnt be visible!
+        return VisualisePotential(Az, Bx, By, Bz, magnetic=True)
+
+    @staticmethod
+    def from_file(filename, magnetic=False):
+        flat_arrs = np.loadtxt(filename, unpack=True)
+        if magnetic:
+            N = round(flat_arrs[0].size ** (1/2))  # Flattened 2D array
+        else:
+            N = round(flat_arrs[0].size ** (1/3))  # flattened 3D array
+        return VisualisePotential(*[arr.reshape(N, N, N) for arr in flat_arrs], magnetic=magnetic)
+
+    @staticmethod
+    def from_solver(solver, magnetic=False):
+        return VisualisePotential(solver.phi, solver.Ex, solver.Ey, solver.Ez, magnetic=magnetic)
+
+
+def main():
+    # Show some examples
+    VisualisePotential.from_point_charge(51)
+    VisualisePotential.from_thin_wire(51)
+
+if __name__ == "__main__":
+    main()
